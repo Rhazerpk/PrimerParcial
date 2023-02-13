c0739791a6654733735545bcbcc59b6522dcdd59
Albert Delgado <bolshoi19booze@gmail.com>
2023-02-13 18:55:24 -0400

    Creando el BLL

---
+++ BLL/LibrosBLL.cs
@@ -0,0 +1,56 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+public class LibrosBLL{
+
+    private Contexto _contexto;
+
+    public LibrosBLL(Contexto contexto)
+    {
+        _contexto = contexto;
+    }
+
+    public bool Existe(int LibroId)
+    {
+        return _contexto.libros.Any(o => o.LibroId == LibroId);
+
+    }
+
+    private bool Insertar(Libros libros)
+    {
+        _contexto.libros.Add(libros);
+        return _contexto.SaveChanges() > 0;
+    }
+
+    private bool Modificar(Libros libros)
+    {
+        _contexto.Entry(libros).State = EntityState.Modified;
+        return _contexto.SaveChanges() > 0;
+
+    }
+
+    public bool Guardar(Libros libros)
+    {
+        if(!Existe(libros.LibroId))
+            return this.Insertar(libros);
+        else
+            return this.Modificar(libros);
+    }
+
+    public Libros? Buscar(int LibroId)
+    {
+        return _contexto.libros
+        .Where(o => o.LibroId == LibroId)
+        .AsNoTracking()
+        .SingleOrDefault();
+    }
+
+    public List<Libros> GetList(Expression<Func<Libros, bool>> Criterio)
+    {
+        return _contexto.libros
+        .AsNoTracking()
+        .Where(Criterio)
+        .ToList();
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 var ConStr = builder.Configuration.GetConnectionString("ConStr");
 builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(ConStr));
+builder.Services.AddScoped<LibrosBLL>();
 
 
 var app = builder.Build();
