8c1ccd7040320b2941fd92309b92ab857e368a40
Albert Delgado <bolshoi19booze@gmail.com>
2023-02-13 20:01:34 -0400

    Everything done

--- Models/Libros.cs
+++ Models/Libros.cs
@@ -4,8 +4,11 @@ public class Libros{
 
     [Key]
 
+    [Required(ErrorMessage ="El libroId es requerido")]
     public int LibroId { get; set; }
+    [Required(ErrorMessage ="El título es requerido")]
     public string? Titulo { get; set; }
+    [Required(ErrorMessage ="El precio es requerido")]
     public double Precio { get; set; }
     
 }
